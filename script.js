34293fa3d7bf3dfda0d26741a44179ae2048d878
Krystle Capuano <krystlecapuano@gmail.com>
2024-04-09 16:45:13 -0400

    "Check 1"

---
+++ script.js
@@ -0,0 +1,22 @@
+//Check 1
+
+const n1 = 10;
+const n2 = 15;
+const n3 = 20;
+const n4 = 5;
+
+const isSum50 = (n1) + (n2) + (n3) + (n4) == 50;
+console.log(isSum50) //true
+
+//Check 2
+
+
+
+
+
+
+
+
+
+
+
