b6cff776bb2f05ab5cea67f3e76c8d29de6b80d4
Krystle Capuano <krystlecapuano@gmail.com>
2024-04-10 16:38:23 -0400

    "Completed part 1"

--- script.js
+++ script.js
@@ -39,46 +39,36 @@ console.log("The answer is " + (chain))
 
 
 
-
 // Check one: add up to 50
 const isSum50 = (n1 + n2 + n3 + n4) == 50;
 console.log("Do these numbers add up to 50 " + isSum50)  
 //-------> Do these numbers add up to 50 true 
 
 
-
 // Check two: at least two odd numbers
-// Here, we use modulus to check if something is odd.
-// Since % 2 is 0 if even and 1 if odd, we can use
-// arithmetic to count the total number of odd numbers.
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+console.log("These numbers are odd - " + (isTwoOdd))
+//------> These numbers are odd - true
 
 // Check three: no number larger than 25
-// This time, we use the OR operator to check
-// if ANY of the numbers is larger than 25.
 const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+console.log("The largest number is " + (isOver25))
+//-------> The largest number is false
+
 
 // Check four: all unique numbers
-// This is long, and there are more efficient
-// ways of handling it with other data structures
-// that we will review later.
 const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+console.log("All unique number are " + (isUnique))
+//-------> All unique number are true
 
-// Here, we put the results into a single variable 
-// for convenience. Note how we negate isOver25 using
-// the ! operator. We could also have tested for 
-// "isUnder25" as an alternative.
+//The results into a single variable 
 const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+console.log("The results are as followed " + (isValid))
+//-------> The results are as followed true
 
-// Finally, log the results.
-console.log(isValid);
 
-// Here's another example of how this COULD be done,
-// but it SHOULD NOT be done this way. As programmers,
-// we break things into small, manageable pieces so that
-// they can be better understood, scaled, and maintained.
-const dontDoThis = ((n1 + n2 + n3 + n4) == 50) && 
-  ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
-  !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
-  (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+//Part 2 Practical Math
+
+const distance = 1500;
+const budget = 175;
 
