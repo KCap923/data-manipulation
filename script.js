009412979fb7a17fc6b989687a7fd3f011e5e80a
Krystle Capuano <krystlecapuano@gmail.com>
2024-04-10 16:19:22 -0400

    "Arithmetic chain"

--- script.js
+++ script.js
@@ -6,18 +6,38 @@ const n4 = 5;
 
 // Numbers divisible by 5
 const divBy5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) === 0;
-console.log("Numbers divisible by 5 " + (divBy5) )
-//------> Numbers divisible by 5 true
+console.log("Numbers divisible by 5 is " + (divBy5) )
+//------> Numbers divisible by 5 is true
 
 //First number larger than last
 const firstLarger = (n1 > n4);
-console.log("First number larger than last " + (firstLarger))
-//-------> First number larger than last true
+console.log("10 larger than 5 is " + (firstLarger))
+//-------> 10 larger than 5 is true true
+
+
+//Arithmetic chain
 
 //Subtract first number from second
 const firstSecond = (n1) - (n2);
-console.log("Subtracting first number from the second " + (firstSecond))
-//-------> Subtracting first number from the second -5
+console.log("10 minus 15 is " + (firstSecond))
+//-------> 10 minus 15 is -5
+
+//Multiply result by third number
+const multiplyResult = (n3) * (firstSecond);
+console.log("Multiplying -5 by 20 is " + (multiplyResult))
+//-------> Multiplying -5 by 20 is -100
+
+//Find remainder 
+const remainder = (multiplyResult) / (n4);
+console.log("Remainder of -100 by 5 is " + (remainder))
+//-------> Remainder of -100 by 5 is -20
+
+//Arithmetic chain PEMDAS 
+const chain = ((n1-n4) * n3) % n4;
+console.log("The answer is " + (chain))
+//------> The answer is 0
+
+
 
 
 // Check one: add up to 50
